9956cddcd8d09fd74f2d3124764d6d04d2d1f347
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-07-15 22:08:00 -0400

    Memoized, string_view, and no references method

--- main.cpp
+++ main.cpp
@@ -1,27 +1,62 @@
+/*
+ * OPTIMIZATIONS TO TEST:
+ *  Storing cache[i][j] using Reference&
+ *  Memoization
+ *  Bottom-up implementation
+ *  std::string_view vs const std::string&
+ *  Vector vs. standard array
+ */
+
 #include <vector>
 #include <string>
 #include <string_view>
 
 int longestCommonSubsequence(std::string text1, std::string text2);
 int dfs(
-        std::string_view text1, std::string_view text2, 
+        std::string_view text1, std::string_view text2,
         int i, int j, int n, int m,
         std::vector<std::vector<int>>& cache);
 
+// ENTRY POINT
 int main(void) {
     return 0;
 }
 
-
+/*
 int longestCommonSubsequence(std::string text1, std::string text2) {
     int n = static_cast<int>(text1.size());
     int m = static_cast<int>(text2.size());
     std::vector<std::vector<int>> cache(n+1, std::vector<int>(m+1, -1));
     return dfs(text1, text2, 0, 0, n, m, cache);
 }
+*/
 
+/* Memoized recursion
+ * String view
+ * No references used
+ */
+int memo_sv_noref(
+        std::string_view text1,
+        std::string_view text2,
+        int i, int j, int n, int m,
+        std::vector<std::vector<int>>& cache) {
+    if (i == n || j == m)
+        return 0; // unable to evaluate i == j
+    if (cache[i][j] > -1)
+        return cache[i][j];
+    int res;
+    if (text1[i] == text2[j])
+        res = 1 + dfs(text1, text2, i+1, j+1, n, m, cache);
+    else
+        res = std::max(dfs(text1, text2, i+1, j, n, m, cache),
+                dfs(text1, text2, i, j+1, n, m, cache));
+    cache[i][j] = res;
+    return res;
+}
+
+/*
 int dfs(
-        std::string_view text1, std::string_view text2, 
+        std::string_view text1, std::string_view text2,
         int i, int j, int n, int m,
         std::vector<std::vector<int>>& cache) {
     if (i == n || j == m) {
@@ -35,10 +70,9 @@ int dfs(
     if (text1[i] == text2[j]) {
         res = 1 + dfs(text1, text2, i+1, j+1, n, m, cache);
     } else {
-        res = std::max(dfs(text1, text2, i+1, j, n, m, cache), 
+        res = std::max(dfs(text1, text2, i+1, j, n, m, cache),
                 dfs(text1, text2, i, j+1, n, m, cache));
     }
     cache_val = res;
     return res;
 }
-
